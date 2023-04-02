88cc87e2cdad399946797828c4a6c96a189c24bd
Dailton Durães <dailtonsd10@gmail.com>
2023-04-02 13:46:55 -0300

    feat: Add Routes

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 
+	"github.com/dailtonduraes/golang-crud/src/controller/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -14,4 +16,12 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 	fmt.Println(os.Getenv("TESTE"))
+
+	router := gin.Default()
+
+	routes.InitRoute(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,11 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/dailtonduraes/golang-crud/src/configuration/rest_err"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
-
+	err := rest_err.NewBadRequestError("Você chamou a rota de forma errada")
+	c.JSON(err.Code, err)
 }
